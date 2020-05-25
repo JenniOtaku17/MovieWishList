069d70025a4c3815ecc63da698b25dd762652a2d
JennyOtaku17 <20175329@itla.edu.do>
2020-05-24 21:05:45 -0600

    Version 0.0.1

---
+++ WishList/WishList/application/views/User/login.php
@@ -0,0 +1,44 @@
+<div class="container">
+<div style="background-color: #fff; padding: 2%; margin-top:8%;">
+  <h2 style="text-align: center;">Welcome to the Login Page</h2>
+  
+  <?= form_open('user/processlogin') ?>
+  <div class="form-group">
+    <?php
+
+        $email= array(
+            'name' => 'email',
+            'placeholder' => 'Please enter you email',
+            'type' => 'email',
+            'class' => 'form-control'
+        );
+        $password= array(
+            'name' => 'password',
+            'placeholder' => 'Please enter you password',
+            'type' => 'password',
+            'class' => 'form-control'
+        );
+
+        ?>
+
+        <?php
+            echo $this->session->flashdata("error");
+        ?><br>
+
+        <?= form_label('Email:', 'email') ?>
+        <?= form_input($email) ?>
+        <br>
+        <?= form_label('Password:', 'password') ?>
+        <?= form_input($password) ?>
+        <br>
+        
+        <div class="col text-center">
+        <?= form_submit('', 'Log In', 'class="btn btn-primary"') ?>
+        </div>
+
+  </div>
+  <?= form_close() ?>
+    </div>
+    </div>
+</body>
+</html>
---
+++ WishList/WishList/application/views/User/register.php
@@ -0,0 +1,54 @@
+
+<div class="container">
+<div style="background-color: #fff; padding: 2%; margin-top:8%;">
+  <h2 style="text-align: center;">Sign UP</h2>
+  
+  <?= form_open('user/store') ?>
+  <div class="form-group">
+    <?php
+        $name= array(
+            'name' => 'name',
+            'placeholder' => 'Please enter you name',
+            'class' => 'form-control'
+        );
+        $email= array(
+            'name' => 'email',
+            'placeholder' => 'Please enter you email',
+            'type' => 'email',
+            'class' => 'form-control'
+        );
+        $password= array(
+            'name' => 'password',
+            'placeholder' => 'Please enter you password',
+            'type' => 'password',
+            'class' => 'form-control'
+        );
+        $confirm= array(
+            'name' => 'confirm',
+            'placeholder' => 'Please confirm you password',
+            'type' => 'password',
+            'class' => 'form-control'
+        );
+        ?>
+
+        <?= form_label('Name:', 'name') ?>
+        <?= form_input($name) ?>
+        <br>
+        <?= form_label('Email:', 'email') ?>
+        <?= form_input($email) ?>
+        <br>
+        <?= form_label('Password:', 'password') ?>
+        <?= form_input($password) ?>
+        <br>
+        <?= form_label('Confirm Password:', 'confirm') ?>
+        <?= form_input($confirm) ?>
+        <br>
+
+        <div class="col text-center">
+        <?= form_submit('', 'Register', 'class="btn btn-primary"') ?>
+        </div>
+  </div>
+  <?= form_close() ?>
+
+</body>
+</html>
